c5a0723d986aca2856c83d364bee1ea7ee2ca169
Aaris-Kazi <aariskazi@gmail.com>
2021-07-26 01:17:58 +0530

    Task 4 is done

--- Task4/task4.py
+++ Task4/task4.py
@@ -35,25 +35,61 @@ def for_unique(list1):
     except Exception:
         pass
     return unique_list
+
+def unique_list(list1):
+    i= 0
+    unique_list = []
+    try:
+        while i <= len(list1):
+            unique_list.append(list1[i])
+            i += 2
+    except Exception:
+        pass
+    return unique_list
+try:
+    PATH = r'Qualifying\chromedriver.exe'
+    driver = webdriver.Chrome(PATH)
+    driver.get('https://www.abeergroup.com/ksa/Doctors.aspx')
+    dev = driver.title
+    print(dev)
+    name = driver.find_elements_by_class_name('Drname.ng-binding')
+    htag = driver.find_elements_by_tag_name('strong')
+    des = driver.find_elements_by_class_name('DrDept.ng-binding')
+    design, location = [], []
+    hospitals = []
+    for i in des:
+        design.append(i.text)
+    design= filtersx(design)
+    design = unique_list(design)
+    # print(len(design))
+    doctor_name= for_unique(name)
+    # print(len(doctor_name))
+    hospital = for_unique(htag)
+    # print(len(hospital))
+    for i in hospital:
+        # print(i)
+        if '-' in i:
+            hospital_name, loc = i.split('-')
+            location.append(loc)
+            hospitals.append(hospital_name)
+        else:
+            hospital_name, loc = i.split('Ruwais')
+            location.append('Ruwais')
+            hospitals.append(hospital_name)
+    print(len(location))
+    print(len(hospitals))
     
-PATH = r'Qualifying\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
-driver.get('https://www.abeergroup.com/ksa/Doctors.aspx')
-dev = driver.title
-print(dev)
-name = driver.find_elements_by_class_name('Drname.ng-binding')
-htag = driver.find_elements_by_tag_name('strong')
-# hospital = driver.find_elements_by_class_name('DrDesignation.ng-binding')
-des = driver.find_elements_by_class_name('DrDept.ng-binding')
-design = []
-for i in des:
-    design.append(i.text)
-design= filtersx(design)
-for i in design:
-    print(i)
-doctor_name= for_unique(name)
-print(len(doctor_name))
-hospital = for_unique(htag)
-print(len(hospital))
-driver.quit()
-driver.close()
+    driver.quit()
+    driver.close()
+except Exception:
+    pass
+
+
+df= pd.DataFrame({
+    "Name": doctor_name,
+    "Speciality": design,
+    "Location": location,
+    "Hospitals": hospitals,
+    # "Phone Number": doc_price
+})
+df.to_csv('doctors_ksa.csv')
