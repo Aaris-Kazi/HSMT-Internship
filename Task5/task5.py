f4ccc6bafb7f09207bd4464bd8b2d68a4a45e726
Aaris-Kazi <aariskazi@gmail.com>
2021-08-03 01:00:15 +0530

    task 5 some changes

--- Task5/task5.py
+++ Task5/task5.py
@@ -13,7 +13,9 @@ manufature = []
 consume_type = []
 med_names = []
 brand_name = []
-
+price = []
+presc = []
+units = []
 PATH = r'Qualifying\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 driver.get('https://www.cipla.com/generic-product-list#antacid')
@@ -32,30 +34,33 @@ time.sleep(3)
 driver.close()
 driver.quit()
 
-i = 1
 for i in range(len(td)):
     if i ==0:
         manufature.append('CIPLA')
         brand_name.append(td[i])
         med_names.append(td[i+1])
         consume_type.append(td[i+2])
+        presc.append('Y')
+        units.append('1')
+        price.append(np.nan)
 
     if i%3 == 0:
         manufature.append('CIPLA')
         brand_name.append(td[i])
         med_names.append(td[i+1])
         consume_type.append(td[i+2])
+        presc.append('Y')
+        units.append('1')
+        price.append(np.nan)
 
 df = pd.DataFrame({
     "medName": brand_name,
-    'Manufacturer': manufature,
+    'manufacturer': manufature,
     "Molecules": med_names,
-    "Dosage Form": consume_type
+    "Dosage Form": consume_type,
+    'Prescription Required': presc,
+    'Units in Pack': units,
+    'Price': price
 })
 df.to_csv('cipla1.csv')
 
-
-# df = pd.DataFrame({
-#     "Data": td
-# })
-# df.to_csv('med_data.csv')
