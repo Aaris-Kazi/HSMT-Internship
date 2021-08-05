54be0e14c664b97377dc87fdb49b2c47fdc4cfbb
Aaris-Kazi <aariskazi@gmail.com>
2021-08-05 15:30:55 +0530

    small changes

--- Task5/filters.py
+++ Task5/filters.py
@@ -4,25 +4,29 @@ consume_type = []
 med_names = []
 brand_name = []
 
-df = pd.read_csv('med_data.csv')
-print(len(df))
-i = 1
-for i in range(len(df)):
-    if i ==0:
-        brand_name.append(df.iloc[i].values)
-        med_names.append(df.iloc[i+1].values)
-        consume_type.append(df.iloc[i+2].values)
+def containsNumber(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
 
-    if i%3 == 0:
-        # print(df.iloc[i])
-        brand_name.append(df.iloc[i].values)
-        med_names.append(df.iloc[i+1].values)
-        consume_type.append(df.iloc[i+2].values)
-# print(consume_type)
-# print(df.iloc[6])
-df = pd.DataFrame({
-    "Brand Name": brand_name,
-    "Molecules": med_names,
-    "Dosage Form": consume_type
-})
-df.to_csv('cipla.csv')
+df = pd.read_csv('cipla1.csv')
+print(len(df))
+
+
+for i in df['medName']:
+    j = containsNumber(i)
+    print(j)
+
+# i = 1
+# for i in range(len(df)):
+#     if i ==0:
+#         brand_name.append(df.iloc[i].values)
+#         med_names.append(df.iloc[i+1].values)
+#         consume_type.append(df.iloc[i+2].values)
+
+#     if i%3 == 0:
+#         # print(df.iloc[i])
+#         brand_name.append(df.iloc[i].values)
+#         med_names.append(df.iloc[i+1].values)
+#         consume_type.append(df.iloc[i+2].values)
