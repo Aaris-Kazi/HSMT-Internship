51560cafb9f12988afa8a5f1c1fe2182d39d3a05
Aaris-Kazi <aariskazi@gmail.com>
2021-08-25 12:04:52 +0530

    Some of the new implementation

---
+++ JustDial_Hospitals_Scraper.py
@@ -0,0 +1,139 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
+
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+
+driver.get("https://www.justdial.com/Mumbai/Hospitals/nct-10253670")
+link = []
+NA = "NaN"
+# '<span class="lng_cont_name">Hcg Ics Khubchandani Cancer..</span>'
+
+def strings_to_num(argument):
+    switcher = {
+        'dc': '+',
+        'fe': '(',
+        'hg': ')',
+        'ba': '-',
+        'acb': '0',
+        'yz': '1',
+        'wx': '2',
+        'vu': '3',
+        'ts': '4',
+        'rq': '5',
+        'po': '6',
+        'nm': '7',
+        'lk': '8',
+        'ji': '9'
+    }
+    return switcher.get(argument, "nothing")
+
+hosps = driver.find_elements_by_class_name("store-name > span > a")
+for hosp in hosps:
+    link.append(hosp.get_attribute("href"))
+
+# ---------Ignore-------------
+# link = ["https://www.justdial.com/Mumbai/Thunga-Hospital-Pvt-Ltd-TMRCT-Near-Jsw-Ltd-Takinaka-Tarapur-MIDC/022PXX22-XX22-170814131518-J5G6_BZDET?xid=TXVtYmFpIFB1YmxpYyBIb3NwaXRhbHM=",]
+# link = ['https://www.justdial.com/Mumbai/Srv-Mamata-Hospital-Near-Omkar-International-School-Next-to-ICICI-Bank-Dombivli-East/022P1152424_BZDET?xid=TXVtYmFpIFB1YmxpYyBIb3NwaXRhbHM=']
+# link = ['https://www.justdial.com/Mumbai/Srv-Hospital-Goregaon-Near-Jawahar-Nagar-Phatak-Jawahar-Nagar-goregaon-West/022PXX22-XX22-180706122025-E1F2_BZDET?xid=TXVtYmFpIFB1YmxpYyBIb3NwaXRhbHM=']
+
+for l in link:
+    print(l)
+    driver.get(l)
+    time.sleep(1)
+    city = "Mumbai"
+    state = "Maharashtra"
+    country = "India"
+    type = "Hospital"
+
+    try:
+        name = driver.find_element_by_class_name("rstotle > span > span").text
+        print("Name : ",name)
+    except:
+        name = NA
+        print("Name : ",name)
+
+    try:
+        ratings = driver.find_element_by_class_name("total-rate > span").text
+        print("Rat : ", ratings)
+    except:
+        print("Rat : ", NA)
+
+    try:
+        votes = driver.find_element_by_class_name("rtngsval > span").text
+        print("Votes : ",votes)
+    except:
+        pass
+
+    try:
+        driver.find_element_by_class_name("jd_verified")
+        verified = "Y"
+        print("Verified : ",verified)
+    except:
+        pass
+
+    try:
+        add = driver.find_element_by_id("fulladdress").text
+        print("Address : ",add)
+    except:
+        pass
+
+    try:
+        web = driver.find_elements_by_class_name("mreinfp.comp-text > a")
+        web = ' , '.join([str(elem) for elem in [w.get_attribute("href") for w in web if w.get_attribute("href")]])
+        print("Websites : ",web)
+        # for w in web:
+        #     if w.get_attribute("href"):
+        #         print("Web : ",w.get_attribute("href"))
+    except:
+        pass
+
+    try:
+        bt = driver.find_element_by_id("vhall")
+        driver.execute_script("arguments[0].click();", bt)
+        time.sleep(1)
+        open = driver.find_element_by_id("mhd").text.split(")")[1].split("\n")
+        open = ','.join([str(elem) for elem in open])
+        print("Timings : ",open)
+    except:
+        pass
+
+    try:
+        info = driver.find_element_by_id("qckinf").text
+        amn = info.split("Amenities")[1].split("\n")
+        amn = ','.join([str(elem).lstrip() for elem in amn])
+        print("Amn : ",amn)
+        serv = info.split("Amenities")[0].split("Services")[1].split("\n")
+        serv = ','.join([str(elem).lstrip() for elem in serv])
+        print("Services : ",serv)
+    except:
+        pass
+
+    try:
+        pay = ','.join([str(elem.text) for elem in driver.find_elements_by_class_name('lng_mdpay')])
+        print("Payment : ",pay)
+    except:
+        pass
+
+    try:
+        try:
+            year = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[4]/div[1]/div[8]/ul/li').text
+        except:
+            year = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[4]/div[1]/div[6]/ul/li').text
+        print("Year Established : ",year)
+    except:
+        pass
+
+    try:
+        social = driver.find_element_by_class_name("social_media_icons > a").get_attribute("href")
+        print("Social : ",social)
+    except:pass
+
+    print("------------------------------------------------------")
+
+
+driver.quit()
+
--- Task8/task8.py
+++ Task8/task8.py
@@ -2,6 +2,8 @@
 # https://www.justdial.com/Surat/260/Medical_fil
 # https://www.justdial.com/Lucknow/260/Medical_fil
 # jcl
+# https://www.justdial.com/Surat/Multispeciality-Hospitals/nct-10547585
+# jcn
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -17,155 +19,233 @@ doc_rating = []
 doc_votes = []
 doc_add = []
 doc_contacts =[]
-# try:
-    # def filtersx(x):
-    #     valid_output = []
-    #     for i in x:
-    #         if len(i) == 0:
-    #             valid_output.append('NaN')
-    #         else:
-    #             valid_output.append(i)
-    #     return valid_output
+doc_links =[]
+doc_page =[]
+try:
+    def strings_to_num(argument):
+        switcher = {
+            'dc': '+',
+            'fe': '(',
+            'hg': ')',
+            'ba': '-',
+            'acb': '0',
+            'yz': '1',
+            'wx': '2',
+            'vu': '3',
+            'ts': '4',
+            'rq': '5',
+            'po': '6',
+            'nm': '7',
+            'lk': '8',
+            'ji': '9'
+        }
+        return switcher.get(argument, "nothing")
+    def filtersx(x):
+        valid_output = []
+        for i in x:
+            if len(i) == 0:
+                valid_output.append('NaN')
+            else:
+                valid_output.append(i)
+        return valid_output
 
 
-    # PATH = r'Task1\chromedriver.exe'
-    # driver = webdriver.Chrome(PATH)
-    # driver.get('https://www.justdial.com/Surat/Women-Dermatologists/nct-12101295')
-    # dev = driver.title
-    # print(dev)
-    # def get_details():
+    PATH = r'Task1\chromedriver.exe'
+    driver = webdriver.Chrome(PATH)
 
-    #     name = driver.find_elements_by_class_name('lng_cont_name')
-    #     rating = driver.find_elements_by_class_name('green-box')
-    #     votes = driver.find_elements_by_class_name('lng_vote')
-    #     speciality = driver.find_elements_by_class_name('lng_commn')
-    #     address = driver.find_element_by_class_name('cont_sw_addr')
-    #     element = WebDriverWait(driver, 15).until(
-    #                 # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))
-    #             EC.element_to_be_clickable((By.CLASS_NAME, 'jcl'))
-    #         )
-    #     driver.execute_script("arguments[0].click();", element)
-    #     return name, rating, votes, address
+    driver.get('https://www.justdial.com/Surat/Multispeciality-Hospitals/nct-10547585')
+    dev = driver.title
+    print(dev)
+    def get_details():
+
+        name = driver.find_elements_by_class_name('lng_cont_name')
+        rating = driver.find_elements_by_class_name('green-box')
+        votes = driver.find_elements_by_class_name('lng_vote')
+        speciality = driver.find_elements_by_class_name('lng_commn')
+        address = driver.find_elements_by_class_name('cont_sw_addr')
+        # address = driver.find_elements_by_class_name('cont_fl_addr')
+        contacts = driver.find_elements_by_class_name('mobilesv')
+        storeDetails = driver.find_elements_by_class_name('store-details')
+        links = driver.find_elements_by_class_name('jcn')
+        page_links = []
+        for i in links:
+            x = i.find_element_by_tag_name('a')
+            page_links.append(x.get_attribute('href'))
+        # print(page_links)
+        img_links = driver.find_elements_by_class_name('altImgcls')
+        numbersList = []
+        for i in range(len(storeDetails)):
+            contactList = storeDetails[i].find_elements_by_class_name('mobilesv')
+            myList = []
+
+            for j in range(len(contactList)):
+                myString = contactList[j].get_attribute('class').split("-")[1]
+                myList.append(strings_to_num(myString))
+
+            numbersList.append("".join(myList))
         
-    # for i in range(2):
-    #     names, ratings, votes, specialties, address = get_details()
-    #     for i, j, k, l, m in zip(names, ratings, votes, specialties, address):
-    #         doc_name.append(i.text)
-    #         doc_rating.append(j.text)
-    #         doc_votes.append(k.text)
-    #         doc_spec.append(l.text)
-    #         doc_add.append(m.text)
-    #     element = WebDriverWait(driver, 10).until(
-    #                 # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))
-    #             EC.element_to_be_clickable((By.XPATH, '//*[@id="srchpagination"]/a[3]'))
-    #         )
-    #     driver.execute_script("arguments[0].click();", element)
-    # doc_name = filtersx(doc_name)
-    # doc_rating = filtersx(doc_rating)
-    # doc_votes = filtersx(doc_votes)
-    # doc_spec = filtersx(doc_spec)
-    # doc_add = filtersx(doc_add)
+        return name, rating, votes,speciality, address, numbersList, img_links, page_links
+            
+    for i in range(1):
+        names, ratings, votes, specialties, address, contacts, img_links, page_links = get_details()
+        for i in contacts:
+            doc_contacts.append(i)
+        for i, j, k, l, m, n, o in zip(names, ratings, votes, specialties, address, img_links, page_links):
+            doc_name.append(i.text)
+            doc_rating.append(j.text)
+            doc_votes.append(k.text)
+            doc_spec.append(l.text)
+            doc_add.append(m.text)
+            doc_links.append(n.get_attribute('src'))
+            doc_page.append(o)
+
+        element = WebDriverWait(driver, 10).until(
+                        # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))//*[@id="srchpagination"]/a[3]
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="srchpagination"]/a[11]'))
+        )
+        driver.execute_script("arguments[0].click();", element)
+    doc_name = filtersx(doc_name)
+    doc_rating = filtersx(doc_rating)
+    doc_votes = filtersx(doc_votes)
+    doc_spec = filtersx(doc_spec)
+    doc_add = filtersx(doc_add)
     # print(doc_name)
     # print(doc_rating)
     # print(doc_votes)
     # print(doc_spec)
     # print(doc_add)
-    # print(len(doc_name))
-    # print(len(doc_rating))
-    # print(len(doc_votes))
-    # print(len(doc_spec))
-    # print(len(doc_add))
-    # driver.quit()
-    # driver.close()
-# except Exception as e:
-#     # pass
-#     print(e)
+    # print(doc_links)
+    # print(doc_contacts)
+    print(len(doc_name))
+    print(len(doc_rating))
+    print(len(doc_votes))
+    print(len(doc_spec))
+    print(len(doc_add))
+    print(len(doc_contacts))
+    print(len(doc_links))
+    print(len(doc_page))
+    driver.quit()
+    driver.close()
+except Exception as e:
+    pass
+    # print(e)
 
-def strings_to_num(argument):
-    switcher = {
-        'dc': '+',
-        'fe': '(',
-        'hg': ')',
-        'ba': '-',
-        'acb': '0',
-        'yz': '1',
-        'wx': '2',
-        'vu': '3',
-        'ts': '4',
-        'rq': '5',
-        'po': '6',
-        'nm': '7',
-        'lk': '8',
-        'ji': '9'
-    }
-    return switcher.get(argument, "nothing")
-def filtersx(x):
-    valid_output = []
-    for i in x:
-        if len(i) == 0:
-            valid_output.append('NaN')
-        else:
-            valid_output.append(i)
-    return valid_output
+# def strings_to_num(argument):
+#     switcher = {
+#         'dc': '+',
+#         'fe': '(',
+#         'hg': ')',
+#         'ba': '-',
+#         'acb': '0',
+#         'yz': '1',
+#         'wx': '2',
+#         'vu': '3',
+#         'ts': '4',
+#         'rq': '5',
+#         'po': '6',
+#         'nm': '7',
+#         'lk': '8',
+#         'ji': '9'
+#     }
+#     return switcher.get(argument, "nothing")
+# def filtersx(x):
+#     valid_output = []
+#     for i in x:
+#         if len(i) == 0:
+#             valid_output.append('NaN')
+#         else:
+#             valid_output.append(i)
+#     return valid_output
 
 
-PATH = r'Task1\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
+# PATH = r'Task1\chromedriver.exe'
+# driver = webdriver.Chrome(PATH)
 
-driver.get('https://www.justdial.com/Surat/Women-Dermatologists/nct-12101295')
-dev = driver.title
-print(dev)
-def get_details():
+# driver.get('https://www.justdial.com/Surat/Multispeciality-Hospitals/nct-10547585')
+# dev = driver.title
+# print(dev)
+# def get_details():
 
-    name = driver.find_elements_by_class_name('lng_cont_name')
-    rating = driver.find_elements_by_class_name('green-box')
-    votes = driver.find_elements_by_class_name('lng_vote')
-    speciality = driver.find_elements_by_class_name('lng_commn')
-    address = driver.find_elements_by_class_name('cont_sw_addr')
-    contacts = driver.find_elements_by_class_name('mobilesv')
-    # element = WebDriverWait(driver, 15).until(
-    #                 # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))
-    #         EC.element_to_be_clickable((By.CLASS_NAME, 'jcl'))
-    # )
-    # driver.execute_script("arguments[0].click();", element)
-    # return name, rating, votes, address
-    return name, rating, votes,speciality, address, contacts
+#     name = driver.find_elements_by_class_name('lng_cont_name')
+#     rating = driver.find_elements_by_class_name('green-box')
+#     votes = driver.find_elements_by_class_name('lng_vote')
+#     speciality = driver.find_elements_by_class_name('lng_commn')
+#     address = driver.find_elements_by_class_name('cont_sw_addr')
+#     # address = driver.find_elements_by_class_name('cont_fl_addr')
+#     contacts = driver.find_elements_by_class_name('mobilesv')
+#     storeDetails = driver.find_elements_by_class_name('store-details')
+#     links = driver.find_elements_by_class_name('jcn')
+#     page_links = []
+#     for i in links:
+#         x = i.find_element_by_tag_name('a')
+#         page_links.append(x.get_attribute('href'))
+#     # print(page_links)
+#     img_links = driver.find_elements_by_class_name('altImgcls')
+#     numbersList = []
+#     for i in range(len(storeDetails)):
+#         contactList = storeDetails[i].find_elements_by_class_name('mobilesv')
+#         myList = []
+
+#         for j in range(len(contactList)):
+#             myString = contactList[j].get_attribute('class').split("-")[1]
+#             myList.append(strings_to_num(myString))
+
+#         numbersList.append("".join(myList))
+    
+#     return name, rating, votes,speciality, address, numbersList, img_links, page_links
         
-for i in range(2):
-    names, ratings, votes, specialties, address, contacts = get_details()
-    for i, j, k, l, m, n in zip(names, ratings, votes, specialties, address, contacts):
-        doc_name.append(i.text)
-        doc_rating.append(j.text)
-        doc_votes.append(k.text)
-        doc_spec.append(l.text)
-        doc_add.append(m.text)
-    element = WebDriverWait(driver, 10).until(
-                    # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))//*[@id="srchpagination"]/a[3]
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="srchpagination"]/a[3]'))
-    )
-    driver.execute_script("arguments[0].click();", element)
-doc_name = filtersx(doc_name)
-doc_rating = filtersx(doc_rating)
-doc_votes = filtersx(doc_votes)
-doc_spec = filtersx(doc_spec)
-doc_add = filtersx(doc_add)
-print(doc_name)
-print(doc_rating)
-print(doc_votes)
-print(doc_spec)
-print(doc_add)
-print(len(doc_name))
-print(len(doc_rating))
-print(len(doc_votes))
-print(len(doc_spec))
-print(len(doc_add))
-driver.quit()
-driver.close()
+# for i in range(2):
+#     names, ratings, votes, specialties, address, contacts, img_links, page_links = get_details()
+#     for i in contacts:
+#         doc_contacts.append(i)
+#     for i, j, k, l, m, n, o in zip(names, ratings, votes, specialties, address, img_links, page_links):
+#         doc_name.append(i.text)
+#         doc_rating.append(j.text)
+#         doc_votes.append(k.text)
+#         doc_spec.append(l.text)
+#         doc_add.append(m.text)
+#         doc_links.append(n.get_attribute('src'))
+#         doc_page.append(o)
+#         # print("".join(n))
+#         # conctact = contacts[n].get_attribute('class').split("-")[1]
+#         # doc_contacts.append(strings_to_num(conctact))
+
+#     # element = WebDriverWait(driver, 10).until(
+#     #                 # EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-nextPage'))//*[@id="srchpagination"]/a[3]
+#     #         EC.element_to_be_clickable((By.XPATH, '//*[@id="srchpagination"]/a[3]'))
+#     # )
+#     # driver.execute_script("arguments[0].click();", element)
+# doc_name = filtersx(doc_name)
+# doc_rating = filtersx(doc_rating)
+# doc_votes = filtersx(doc_votes)
+# doc_spec = filtersx(doc_spec)
+# doc_add = filtersx(doc_add)
+# # print(doc_name)
+# # print(doc_rating)
+# # print(doc_votes)
+# # print(doc_spec)
+# # print(doc_add)
+# # print(doc_links)
+# # print(doc_contacts)
+# print(len(doc_name))
+# print(len(doc_rating))
+# print(len(doc_votes))
+# print(len(doc_spec))
+# print(len(doc_add))
+# print(len(doc_contacts))
+# print(len(doc_links))
+# print(len(doc_page))
+# driver.quit()
+# driver.close()
 # df= pd.DataFrame({
+#     'Photo': doc_links,
 #     "Name": doc_name,
+#     "Speciality": doc_spec,
+#     "Address": doc_add,
+#     "Contacts": doc_contacts,
+#     "Page Link": doc_page,
 #     "Raitngs": doc_rating,
 #     "Votes": doc_votes
 # })
 # df.to_csv('surat_doctors.csv')
 
-# //*[@id="srchpagination"]/a[3]
