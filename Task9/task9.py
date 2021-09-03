37362398b70166f82f740d3c729bcbab7d3aa7cd
Aaris-Kazi <aariskazi@gmail.com>
2021-09-03 17:33:15 +0530

    Some more changes

--- Task9/task9.py
+++ Task9/task9.py
@@ -6,13 +6,48 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.common.exceptions import StaleElementReferenceException
 import pandas as pd
-
-PATH = r'Task1\chromedriver.exe'
+# Procedure/surgery name , Cost , Total days , Top hospitals, Top doctors
+doc_name = []
+hopsital = []
+days = []
+cost = []
+surger_name = []
+PATH = r'chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-driver.get('https://www.vaidam.com/cost')
+driver.get('https://www.vaidam.com/cost/24-hour-holter-monitoring-cost-in-india')
 dev = driver.title
 print(dev)
+procedure = driver.find_element_by_class_name('title')
+price = driver.find_element_by_xpath('//*[@id="treatment-cost"]/div/div/div/div[2]/div/table/tbody/tr/td[1]/span[2]')
+tdays = driver.find_element_by_xpath('//*[@id="treatment-cost"]/div/div/div/div[2]/div/table/tbody/tr/td[5]/span[2]')
+print(procedure.text)
+print(price.text)
+print(tdays.text)
+for i in range(4):
+    name = driver.find_element_by_xpath('//*[@id="top-doctors"]/div/div/div[2]/div['+str(i+1)+']/div[2]/h3/a')
+    spec = driver.find_element_by_xpath('//*[@id="top-doctors"]/div/div/div[2]/div['+str(i+1)+']/div[2]/p[1]')
+    exp_loc = driver.find_element_by_xpath('//*[@id="top-doctors"]/div/div/div[2]/div['+str(i+1)+']/div[2]/p[2]')
+    hosp = driver.find_element_by_xpath('//*[@id="top-hospitals"]/div/div/div[2]/div['+str(i+1)+']/div[2]/h3/a')
+    # print(name.text)
+    print(spec.text)
+    print(exp_loc.text)
+    print(hosp.text)
+    doc_name.append(name.text)
+    hopsital.append(hosp.text)
+    days.append(tdays.text)
+    cost.append(price.text)
+    surger_name.append(procedure.text)
+
 time.sleep(5)
 driver.close()
 driver.quit()
+
+
+
+# //*[@id="top-doctors"]/div/div/div[2]/div[1]/div[2]/h3/a
+# //*[@id="top-doctors"]/div/div/div[2]/div[2]/div[2]/h3/a
+
+# //*[@id="top-doctors"]/div/div/div[2]/div[2]/div[2]/p[1]
+
+# //*[@id="top-doctors"]/div/div/div[2]/div[2]/div[2]/p[2]
