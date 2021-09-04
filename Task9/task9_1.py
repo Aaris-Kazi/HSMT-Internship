f5d6d72857195b837810c012abae1b34e111f7fa
Aaris-Kazi <aariskazi@gmail.com>
2021-09-04 23:22:12 +0530

    This is much better

---
+++ Task9/task9_1.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.common.exceptions import StaleElementReferenceException
+import pandas as pd
+contacts = []
+links = []
+# PATH = r'msedgedriver.exe'
+# driver = webdriver.Edge(PATH)
+PATH = r'chromedriver.exe'
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome(PATH, options=options)
+# driver = webdriver.Chrome(PATH)
+url = 'https://www.indiahealthcare.org/?speciality=Urology'
+driver.get(url)
+dev = driver.title
+print(dev)
+name = driver.find_elements_by_class_name('hospital-name')
+location = driver.find_elements_by_class_name('hospital-location')
+# for names in name:
+#     name.click()
+# for i in range(len(location)):
+#     contact = driver.find_element_by_xpath('//*[@id="searched_results"]/div/div['+str(i+1)+']/div/p[2]').text
+#     # contact = str(contact).replace('Contact - ', '')
+#     contacts.append(contact)
+#     print(contact)
+
+for i, j in zip(name, location):
+    print(i.text)
+    print(j.text)
+time.sleep(5)
+driver.close()
+driver.quit()
+# //*[@id="searched_results"]/div/div[2]/div/p[2]
+# //*[@id="pagination"]/li[1]/a
