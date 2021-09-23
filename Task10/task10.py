c8452352289c61a27177b057f0ca050b55399b8c
Aaris-Kazi <aariskazi@gmail.com>
2021-09-23 10:59:12 +0530

    good changes

---
+++ Task10/task10.py
@@ -0,0 +1,36 @@
+# https://www.healthgrades.com/hospital-directory/ca-california/san-jose
+# https://www.healthgrades.com/hospital-directory/ca-california/san-diego
+# https://www.healthgrades.com/hospital-directory/tx-texas/dallas
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+PATH = 'chromedriver.exe'
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(PATH, options=options)
+url = 'https://www.healthgrades.com/hospital-directory/ca-california/san-jose'
+driver.get(url)
+# _2YLqr
+i = 0
+tabs = driver.find_elements_by_class_name('_2YLqr')
+for tab in tabs:
+    # tab.click()
+    time.sleep(1)
+    driver.execute_script("arguments[0].click();", tab)
+    driver.switch_to.window(driver.window_handles[i])
+    time.sleep(1)
+    if i> 1:
+        driver.close()
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[0])
+    i +=1
+time.sleep(3)
+driver.close()
+driver.quit()
--- Task9/filtering.py
+++ Task9/filtering.py
@@ -1,6 +1,6 @@
 import pandas as pd
 links, price_usd, price_inr, room_count, km, rating, hotel_name, hosp = [], [], [], [], [], [], [], []
-df = pd.read_csv('Task9\hotel.csv')
+df = pd.read_csv('hotels_hosp.csv')
 # print(df)
 def gsplit(value):
     m,n = value.split('\n')
@@ -33,31 +33,39 @@ for i in range(len(df)):
             m = gsplit(m)
             price_inr.append(m)
             # print(m)
-            # m, n = n.split("\nPRICE RANGE (USD):")
-            # m = gsplit(m)
-            # print(m)
             m, n = n.split("\nHOTELS LINK:")
             m = gsplit(m)
             price_usd.append(m)
             # print(m)
             n = gsplit(n)
-            links.append(n)
+            if n == '' or n == None:
+                links.append('NAN')
+            else:
+                links.append(n)
             # print(n)
             # print(df.iloc[i].values)
             
     except Exception as e:
         # print(e)
         pass
-df = pd.DataFrame({
-    'Hospital': hosp,
-    'Hotels': hotel_name,
-    'Ratings': rating,
-    'KM Range': km,
-    'Room Count': room_count,
-    'Price USD': price_usd,
-    'Price INR': price_inr,
-    'Links': links
-})
-# # df = pd.DataFrame(dic)
-df.to_csv("hosp_hotel1.csv", mode="a")
+print(len(hosp))
+print(len(hotel_name))
+print(len(rating))
+print(len(km))
+print(len(room_count))
+print(len(price_usd))
+print(len(price_inr))
+print(len(links))
+# df = pd.DataFrame({
+#     'Hospital': hosp,
+#     'Hotels': hotel_name,
+#     'Ratings': rating,
+#     'KM Range': km,
+#     'Room Count': room_count,
+#     'Price USD': price_usd,
+#     'Price INR': price_inr,
+#     # 'Links': links
+# })
+# df.to_csv("hosp_hotel4.csv", mode="a")
+# df.to_csv("hosp_hotel3.csv", mode="a", header= False)
 
