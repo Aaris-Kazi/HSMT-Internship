c3f0efe97b607d51a9af15ad2ea30dd41b397431
Aaris-Kazi <aariskazi@gmail.com>
2021-10-09 15:41:19 +0530

    just stack up

---
+++ task11/filters.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('task11\chat3.csv')
+# print(df['Questions'])
+# print(df['Options'])
+q, o, d= [],[],[]
+for i, j in zip(df['Questions'], df['Options']):
+    d.append('COVID-19')
+    q.append(i)
+    o.append(j)
+jf = pd.DataFrame({
+    "Disease": d,
+    "Questions":q,
+    "Options": o
+})
+jf.to_csv('mfine_chat.csv')
