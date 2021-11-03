c6ffca6d2f6b658da84e329b81269e9340b0210c
Aaris-Kazi <aariskazi@gmail.com>
2021-11-03 12:16:18 +0530

    c data

--- Task13/task_13.py
+++ Task13/task_13.py
@@ -13,7 +13,7 @@ drug_facts = []
 data_all = []
 Link_to_profile=[]
 
-url = "https://www.healthgrades.com/drugs/fda/a-z/alpha-b"
+url = "https://www.healthgrades.com/drugs/fda/a-z/alpha-c"
 resp = requests.get(url)
 resp.status_code
 resp.reason
