9d87dbff1a2a4b83e004f630dd36dd1ae1b3ea3a
Aaris-Kazi <aariskazi@gmail.com>
2021-12-02 16:50:17 +0530

    first zocdoc

--- Task14/task14.py
+++ Task14/task14.py
@@ -16,35 +16,49 @@ driver = webdriver.Chrome(PATH, options=options)
 
 url = 'https://www.zocdoc.com/'
 driver.get(url)
-try:
-    search =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/div/div/div[1]/div/div/div[2]/input')
-    search.click()
-    time.sleep(1)
-    opt =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/div/div/div[1]/div/div/div[4]/div[2]/div')
-    opt.click()
-    time.sleep(1)
-    opt =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/button')
-    opt.click()
-    time.sleep(3)
-    # name =driver.find_element_by_xpath('//*[@id="main"]/div/main/div/div[2]/div/div[2]/div/div/section/article[2]/div[1]/div[1]/div/div[1]/div/div[2]/div[1]/a')
-    # print(name.text)
+name, designation, rate, rate_count = [],[],[],[]
+def looping():
     names = driver.find_elements_by_class_name('cmGgZN')
     practitioner = driver.find_elements_by_class_name('jyfAkd')
     rating = driver.find_elements_by_class_name('dWnIDu')
     count_rating = driver.find_elements_by_class_name('ehwwpF')
-    next = driver.find_element_by_xpath('//*[@id="main"]/div/main/div/nav/span[2]/a')
+    for a,b,c,d in zip(names, practitioner, rating, count_rating):
+        name.append(a.text)
+        designation.append(b.text)
+        rate.append(c.text)
+        rate_count.append(d.text)
+        print(a.text)
+    print(len(name),len(designation),len(rate),len(rate_count))
+
+search =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/div/div/div[1]/div/div/div[2]/input')
+search.click()
+time.sleep(1)
+opt =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/div/div/div[1]/div/div/div[4]/div[2]/div')
+opt.click()
+time.sleep(1)
+opt =driver.find_element_by_xpath('//*[@id="main"]/div[1]/main/div[1]/section/div/div/div/form/div/button')
+opt.click()
+time.sleep(3)
+    # looping()
+    # name =driver.find_element_by_xpath('//*[@id="main"]/div/main/div/div[2]/div/div[2]/div/div/section/article[2]/div[1]/div[1]/div/div[1]/div/div[2]/div[1]/a')
+    # print(name.text)
+for i in range(2,10):
+    time.sleep(5)
+    looping()
+    next = driver.find_element_by_xpath('//*[@id="main"]/div/main/div/nav/span['+str(i)+']/a')
+    next.click()
     # //*[@id="main"]/div/main/div/nav/span[3]/a
-    for i in names:
-        print(i.text)
-    for i in practitioner:
-        print(i.text)
-    for i in rating:
-        print(i.text)
-    for i in count_rating:
-        print(i.text)
+    
     # //*[@id="main"]/div/main/div/div[2]/div/div[2]/div/div/section/article[3]/div[1]/div[1]/div/div[1]/div/div[2]/div[1]/a
-except Exception as e:
-    pass
+
 time.sleep(5)
 driver.close()
 driver.quit()
+
+df = pd.DataFrame({
+    "Name": name,
+    "Practitioner": designation,
+    "Ratings": rate,
+    "Raring Counts": rate_count,
+})
+df.to_csv('zocdoc_aaris.csv', header = False, mode='a')
